e31dcd4cdb9f4f53aa76345c3bd378a9050d0dee
vaidyabandhu <support@vaidyabandhu.com>
2025-11-15 18:38:39 +0530

    Update MembershipCardBenefits.js

--- src/components/sections/home/MembershipCardBenefits.js
+++ src/components/sections/home/MembershipCardBenefits.js
@@ -21,7 +21,7 @@ const MembershipCardBenefits = () => {
     "We help you choose the right doctors — connect with us via call, WhatsApp, email, or visit us.",
   ];
 
-  const membershipCardImage = "assets/img/membership-card.jpg";
+  const membershipCardImage = "assets/img/card-vbb.jpeg";
 
   return (
     <div
